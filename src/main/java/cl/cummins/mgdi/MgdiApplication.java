a684a8a4eb078545050d6d80e09c0912bb0a5073
aaron-cummins <aaron.zuniga@cummins.cl>
2022-06-08 11:20:43 -0400

    Initial commit

---
+++ src/main/java/cl/cummins/mgdi/MgdiApplication.java
@@ -0,0 +1,13 @@
+package cl.cummins.mgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgdiApplication.class, args);
+	}
+
+}
