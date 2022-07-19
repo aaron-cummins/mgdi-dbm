336d27951340c5230962b072a700e7f3071fd4a3
aaron-cummins <aaron.zuniga@cummins.cl>
2022-07-19 10:51:06 -0400

    modifica Region

--- src/main/java/cl/cummins/mgdi/service/RegionService.java
+++ src/main/java/cl/cummins/mgdi/service/RegionService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class RegionService implements ICRUDSevice<Region>{
+public class RegionService implements ICRUDService<Region>{
 
     @Autowired
     private IRegionRepository regionRepo;
