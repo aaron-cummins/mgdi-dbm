d9b444d8fc19fc042bb053b2809b47a3920bccb9
Xtian <cristian.reyes@mayor.cl>
2022-09-07 08:58:20 -0300

    Actualizacion

--- src/test/java/cl/cummins/mgdi/unittest/MgdiApplicationTests.java
+++ src/test/java/cl/cummins/mgdi/unittest/MgdiApplicationTests.java
@@ -1,18 +1,17 @@
 package cl.cummins.mgdi.unittest;
 
+import cl.cummins.mgdi.MgdiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(classes = MgdiApplicationTests.class)
-@SpringBootTest()
+@ContextConfiguration(classes = MgdiApplication.class)
+@SpringBootTest
 class MgdiApplicationTests {
 
 	@Test
 	void contextLoads() {
 		System.out.println("Prueba");
 	}
-
-
 
 }
